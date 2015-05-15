35009ea7cd7fbc04977d9ace1bb907cde002bb1a
mamaker <madhuvasudevan@hotmail.com>
2015-05-14 21:48:49 -0500

    modified

---
+++ FingersTheClown_modified/FunnyFunny.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FingersTheClown_modified
+{
+    class FunnyFunny : IClown
+    {
+        private string funnyThingIHave;
+        public FunnyFunny(string funnyThingIHave)
+        {
+            this.funnyThingIHave = funnyThingIHave;
+        }
+        
+        public string FunnyThingIHave
+        {
+            get { return "Hi kids! I have " + funnyThingIHave; }
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine(this.FunnyThingIHave);
+        }
+    }
+}
---
+++ FingersTheClown_modified/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FingersTheClown_modified
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScaryScary fingersTheClown = new ScaryScary("big shoes", 14);
+            FunnyFunny someFunnyClown = fingersTheClown;
+            IScaryClown someOtherScaryClown = someFunnyClown as IScaryClown;// ScaryScary;
+            someOtherScaryClown.Honk();
+            someOtherScaryClown.ScareLittleChildren();
+            Console.ReadKey();
+        }
+    }
+}
